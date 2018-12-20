e32d5258f08a13a9d333c08f1fbce911004aebc1
GerardoAntonioAS <44910433+GerardoantonioAS@users.noreply.github.com>
2018-12-19 23:35:30 -0300

    cambio estructura gramatical R

--- desayuno R.R
+++ desayuno R.R
@@ -22,26 +22,64 @@ agregar función
     return(pan caliente)
     }else{return(paste("undefined"))}}
   
-"calentar sarten"<-function(x){
-  if{(x==masa para panqueque)return(`masa lista`)}
-"rellenar masa"<-function(x){
-  if{(x==masa lista)} return(panqueque relleno)}
   
-"servir"<-function(x){
-  (`panqueque relleno`,`jugo de naranja`,`pan con palta`,`cafe caliente`)
+  "calentar sarten"<-function(x){
+    if (x=="sarten"){
+        return("sarten caliente")
+    }else{
+        return(paste("probar otro elemento",x)) 
+    }
+}
+"preparar"<-function(x,y){
+    if(x=="naranja cortada"){
+        return("jugo de naranja")
+    }else if((x=="harina" && y=="huevos")||(y=="harina" && x=="huevos")){
+        return("masa para panqueque")
+    }else if((x=="masa para panqueque" && y=="sarten caliente")||(y=="masa para panqueque" && x=="sarten caliente")){
+        return("masa lista")
+    }else if((x=="pan caliente" && y=="palta molida")||(y=="pan caliente" && x=="palta molida")){
+        return("pan tostado palta")
+    }else{
+        return("faltan elementos")  
+    }
+}
+
+  "rellenar"<-function(x){
+    if(x=="masa lista"){
+        return("panqueque relleno")
+    }else{
+        return(paste("probar otro elemento ",x)) 
+    }
+}
   
-"cortar"<-function(x){
-  (naranjas,palta)return(`naranja cortada`,`palta cortada`)
+ "moler"<-function(x){
+    if(x=="palta cortada"){
+        return("palta molida")
+    }else{
+        return(paste("No se que hacer con ",x))
+    }
+}
+  "cortar"<-function(x){
+    if(x=="pan"){
+        return("pan cortado")
+    }else if(x=="naranjas"){
+        return("naranja cortada")
+    }else if(x=="palta"){
+        return("palta cortada")
+    }else{
+        return(paste("probar otro elemento ",x))
+    }
+}
+
+   "servir"<-function(x){
+    if((x=="pan con palta" && y=="panqueque relleno" && z=="jugo de naranja" && j=="cafe caliente")){
+        return("bon appetit! disfrutalo")
+    }else{
+        return("intentad nuevamente, la hais liado")
+    }
+}
+
   
-"preparar jugo"<-function(x){
-  (`naranja cortada`)return(`jugo de naranja`)
   
-"calentar"<-function(x){
-  if{x==pan)return(`pan caliente`,`cafe caliente`)else(x==cafe)
-    
-"moler"<-function(x){if
-  (x==palta cortada)return(palta molida)
-"preparar pan con palta"<-function(x){
-  (`pan caliente`,`palta molida`)return(`pan con palta`)
 
 
